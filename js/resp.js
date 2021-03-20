8d069fb297b12e2c3404f21d2d781d88ec53aa0f
[firstname lastname] <helloworldprogrammer612@gmail.com>
2021-03-20 13:27:53 +0530

    Resposive

---
+++ js/resp.js
@@ -0,0 +1,17 @@
+let burger = document.querySelector(".burger");
+let navbar = document.querySelector(".navbar");
+let navlist = document.querySelector(".nav-list");
+let  rightnav = document.querySelector(".rightNav");
+
+
+
+
+burger.addEventListener("click",  () => {
+
+    rightnav.classList.toggle('v-class-resp');
+    navlist.classList.toggle('v-class-resp');
+    navbar.classList.toggle('h-nav-resp');
+
+});
+
+
